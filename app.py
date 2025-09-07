888c5ab8446c9aaa9b0ac5543cef6168a85e7061
R Hasan <inorothers466@gmail.com>
2025-09-07 22:08:45 +0600

    Deploy ready

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request
-import re
+import os
 
 app = Flask(__name__)
+
 
 def check_password_strength(password):
     strength = 0
     if len(password) >= 8:
         strength += 1
-    if re.search(r'[A-Z]', password):
+    if re.search(r"[A-Z]", password):
         strength += 1
-    if re.search(r'[a-z]', password):
+    if re.search(r"[a-z]", password):
         strength += 1
-    if re.search(r'\d', password):
+    if re.search(r"\d", password):
         strength += 1
     if re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
         strength += 1
     return strength
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -31,5 +33,7 @@ def index():
             result = "Strong Password 🟢"
     return render_template("index.html", result=result)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environment.get("port", 5000))
+    app.run(host="0.0.0.0", port=port)
